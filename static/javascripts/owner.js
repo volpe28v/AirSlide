758b3b0e21dcdc3a5a2cafbde45e7ebf3a24cf30
naoki kodama <naoki.koda@gmail.com>
2012-07-22 17:10:42 +0900

    Add slide obj to owner.js.

--- static/javascripts/owner.js
+++ static/javascripts/owner.js
@@ -1,3 +1,20 @@
+var slideList = function(){
+  var file_list = null;
+  var current_no = 0;
+
+  return {
+    set: function(list){
+      file_list = list;
+    },
+    current: function(){
+      return file_list[current_no];
+    },
+    set_current: function(no){
+      current_no = no;
+    }
+  };
+}();
+
 $(function() {
     var socket = new io.connect('/');
     socket.on('connect', function() {
@@ -7,14 +24,18 @@ $(function() {
 
     socket.on('get_list', function(list){
       console.log(list);
+      slideList.set(list);
+
+      socket.emit('select_file',slideList.current());
 
       for(var i = 0; i < list.length; i++){
         var thumb = $('<li/>').append($('<img/>').attr('src', "/data/" + list[i]));
         thumb.click(function(){
-          var file_name = list[i];
+          var no = i;
           return function(){
-            socket.emit('select_file',file_name);
-            console.log(file_name);
+            slideList.set_current(no);
+            socket.emit('select_file',slideList.current());
+            console.log(slideList.current());
           }
         }());
         $('#thumb-list').append(thumb);
