9dc9cb375a10c6e1c49a0c77d9236572f8a5801d
naoki kodama <naoki.koda@gmail.com>
2012-08-13 00:17:13 +0900

    Add a marker of selected thumbnail.

--- static/javascripts/owner.js
+++ static/javascripts/owner.js
@@ -2,34 +2,38 @@ var slideList = function(){
   var dir_name = null;
   var file_list = null;
   var current_no = 0;
-  var action_handler = null;
+  var move_action_handler = null;
+  var select_action_handler = null;
 
   return {
-    set: function(slide_data, handler){
+    set: function(slide_data, move_handler, select_handler){
       current_no = 0;
       file_list = slide_data.list;
       dir_name = slide_data.dir;
-      action_handler = handler;
+      move_action_handler = move_handler;
+      select_action_handler = select_handler;
     },
     current: function(){
       return dir_name + "/" + file_list[current_no];
     },
     set_current: function(no){
       current_no = no;
+      if ( current_no < 0 ){ current_no = 0; }
+      if ( current_no >= file_list.length ){ current_no = file_list.length - 1; }
+
+      select_action_handler(current_no);
     },
     get_by: function(no){
       return dir_name + "/" + file_list[no];
     },
     next: function(){
-      current_no++;
-      if ( current_no >= file_list.length ){ current_no = file_list.length - 1; }
+      this.set_current(current_no + 1);
 
       this._call_action_handler();
       return this.current();
     },
     prev: function(){
-      current_no--;
-      if ( current_no < 0 ){ current_no = 0; }
+      this.set_current(current_no - 1);
 
       this._call_action_handler();
       return this.current();
@@ -37,7 +41,7 @@ var slideList = function(){
     _call_action_handler: function(){
       var start_pos = current_no + 1;
       if ( start_pos > file_list.length - 4 ){ start_pos = file_list.length - 4; }
-      action_handler(start_pos);
+      move_action_handler(start_pos);
     }
   };
 }();
@@ -76,13 +80,20 @@ $(function() {
 
     socket.on('get_list', function(slide_data){
       var list = slide_data.list;
-      slideList.set(slide_data,function(no){
-        $('#slider-code').tinycarousel_move(no);
-      });
+      slideList.set(slide_data,
+        function(no){ // move_handler
+          $('#slider-code').tinycarousel_move(no);
+        },
+        function(no){ // select_hander
+          $('.thumb').addClass("normal-thumb");
+          $('.thumb').removeClass("current-thumb");
+          $('#thumb_' + no).addClass("current-thumb");
+        }
+      );
 
       $('#thumb-list').empty();
       for(var i = 0; i < list.length; i++){
-        var thumb = $('<li/>').append($('<img/>').attr('src', "/data/" + slideList.get_by(i)).addClass("thumb").addClass('pointer-item'));
+        var thumb = $('<li/>').append($('<img/>').attr('src', "/data/" + slideList.get_by(i)).attr('id',"thumb_" + i).addClass("thumb normal-thumb").addClass("pointer-item"));
         thumb.click(function(){
           var no = i;
           return function(){
